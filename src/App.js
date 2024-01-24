77463fe34a988b7e6a3dbf4b9e71ab2ff939e64b
Matvey Zhigalka <mzhigalka@gmail.com>
2024-01-24 11:02:42 +0200

    Received data from backend

--- src/App.js
+++ src/App.js
@@ -3,71 +3,22 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
-const array = [
-  { 
-    title: "Мужские Кроссовки Nike Blazer Mid Suede", 
-    price: 12999, 
-    imageUrl: "/img/sneakers/1.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Nike Air Max 270", 
-    price: 12999, 
-    imageUrl: "/img/sneakers/2.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Nike Blazer Mid Suede", 
-    price: 8499, 
-    imageUrl: "/img/sneakers/3.jpg" 
-  },
-  { 
-    title: "Кроссовки Puma X Aka Boku Future Rider", 
-    price: 8999, 
-    imageUrl: "/img/sneakers/4.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Under Armour Curry 8", 
-    price: 15199, 
-    imageUrl: "/img/sneakers/5.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Nike Kyrie 7", 
-    price: 11299, 
-    imageUrl: "/img/sneakers/6.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Jordan Air Jordan 11", 
-    price: 10799, 
-    imageUrl: "/img/sneakers/7.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Nike LeBron XVIII", 
-    price: 16499, 
-    imageUrl: "/img/sneakers/8.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Nike Lebron XVIII Low", 
-    price: 13999, 
-    imageUrl: "/img/sneakers/9.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Nike Blazer Mid Suede", 
-    price: 8499, 
-    imageUrl: "/img/sneakers/10.jpg" 
-  },
-  { 
-    title: "Кроссовки Puma X Aka Boku Future Rider", 
-    price: 8999, 
-    imageUrl: "/img/sneakers/11.jpg" 
-  },
-  { 
-    title: "Мужские Кроссовки Nike Kyrie Flytrap IV", 
-    price: 11999, 
-    imageUrl: "/img/sneakers/12.jpg" 
-  },
-];
-
 function App() {
   const [cartOpened, setCartOpened] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+
+  // Received data from backend
+  React.useEffect(() => {
+    fetch('https://65b0c894d16d31d11bdd3bd9.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Error while receiving data')
+      });
+  }, [])
 
   return (
     <div className="wrapper clear">
@@ -86,7 +37,7 @@ function App() {
             </div>
           </div>
           <div className="main-cards d-flex justify-between flex-wrap">
-            {array.map((obj) => (
+            {items.map((obj) => (
               <Card 
                 title={obj.title} 
                 price={obj.price} 
