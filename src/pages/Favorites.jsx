900aa925e8952c5fd55276e7cdb196e42c9e152a
Matvey Zhigalka <mzhigalka@gmail.com>
2024-02-01 13:27:24 +0200

    update Favorites component

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,19 @@
 import Card from "../components/Card";
+import { Link } from 'react-router-dom';
 
 export default function Favorites({ items, onAddToFavorite }) {
   return (
     <div className="content p-40">
-      <div className="d-flex justify-between align-center mb-40">
+      <div className="content__favorites d-flex align-center mb-40">
+        <Link to='/'>
+          <div className="content__favorites-block">
+            <img 
+              className="arrow__left d-flex" 
+              src="./img/arrowLeft.svg" 
+              alt="Arrow" 
+            />
+          </div>
+        </Link>
         <h1 className="content__title ">Мои закладки</h1>
       </div>
       <div className="main-cards d-flex justify-between flex-wrap">
