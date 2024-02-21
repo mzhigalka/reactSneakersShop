9a3704e83d07348a5a69115926b0f2887dc3193a
Matvey Zhigalka <mzhigalka@gmail.com>
2024-02-21 13:24:22 +0200

    added pagination for Swiper

--- src/Swiper.js
+++ src/Swiper.js
@@ -1,30 +1,32 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-// import { Pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
+
 import "swiper/css";
+import 'swiper/css/pagination';
 
 export default () => {
   return (
-    <div className='swiper'>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-      >
-        <SwiperSlide>
-            <div className="swiper__inner">
-              <img src="img/slider.svg" alt="Swiper" />
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiper__inner">
-            <img src="img/slider.svg" alt="Swiper" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiper__inner">
-            <img src="img/slider.svg" alt="Swiper" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    <Swiper
+      pagination={true}
+      modules={[Pagination]}
+      spaceBetween={50}
+      slidesPerView={1}
+    >
+      <SwiperSlide>
+        <div className="swiper__inner">
+          <img src="img/slider.svg" alt="Swiper" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="swiper__inner">
+          <img src="img/slider.svg" alt="Swiper" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="swiper__inner">
+          <img src="img/slider.svg" alt="Swiper" />
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 };
